3010c083fe904805b91c73cd201f37a8f1ce148e
Rahul Kumar Pasawan <rahuldhattarwalhh@gmail.com>
2024-09-25 22:24:34 +0530

    Method added

--- BeingTech/src/main/java/com/BeingTech/Dao/IUsersDao.java
+++ BeingTech/src/main/java/com/BeingTech/Dao/IUsersDao.java
@@ -1,5 +1,12 @@
 package com.BeingTech.Dao;
 
-public class IUsersDao {
+import com.BeingTech.Dto.UsersDto;
 
+public interface IUsersDao {
+ 
+	public boolean saveUser(UsersDto user);
+	public UsersDto getUserByEmailAndPassword(String email, String password);
+	public boolean updateUser(UsersDto user);
+	public UsersDto getUserByUserId(int userId);
+		
 }
