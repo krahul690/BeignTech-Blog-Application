0a3dacc88a8a915593b009f39b0b1fafb4235a1e
Rahul Kumar Pasawan <rahuldhattarwalhh@gmail.com>
2024-09-26 22:45:04 +0530

    UserDaoFactory class used in this class

--- BeingTech/src/main/java/com/BeingTech/Service/UserserviceImpl.java
+++ BeingTech/src/main/java/com/BeingTech/Service/UserserviceImpl.java
@@ -1,31 +1,35 @@
 package com.BeingTech.Service;
 
+import com.BeingTech.Dao.IUsersDao;
+import com.BeingTech.DaoFactory.UserDaoFactory;
 import com.BeingTech.Dto.UsersDto;
 
 public class UserserviceImpl implements IUserservice {
 
+	private IUsersDao users;
 	@Override
 	public boolean saveUser(UsersDto user) {
-
-		return false;
+		users = UserDaoFactory.getUsersDao();
+		return users.saveUser(user);
 	}
 
 	@Override
 	public UsersDto getUserByEmailAndPassword(String email, String password) {
-
-		return null;
+		users = UserDaoFactory.getUsersDao();
+		return users.getUserByEmailAndPassword(email, password);
 	}
 
 	@Override
 	public boolean updateUser(UsersDto user) {
-
-		return false;
+		users = UserDaoFactory.getUsersDao();
+		return users.updateUser(user);
 	}
 
 	@Override
 	public UsersDto getUserByUserId(int userId) {
-
-		return null;
+		users = UserDaoFactory.getUsersDao();
+		return users.getUserByUserId(userId);
+		
 	}
 
 }
